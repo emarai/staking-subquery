da4de03a4df229c2e761792d9a6d65f6a4d1149e
seandotau <seandotau@gmail.com>
2023-02-14 22:24:28 +1100

    Change FunctionCall to Function

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -17,6 +17,7 @@ import {NearTransaction, NearBlock, NearAction, Transfer} from "@subql/types-nea
 export async function handleTransaction(transaction: NearTransaction): Promise<void> {
   
   logger.info(`Handling transaction at ${transaction.block_height}`);
+  logger.info(`IN TRANSACTION FUNCTION`);
 
   const transactionRecord = NearTxEntity.create({
     id: `${transaction.block_hash}-${transaction.result.id}`,
@@ -28,7 +29,8 @@ export async function handleTransaction(transaction: NearTransaction): Promise<v
   await transactionRecord.save();
 }
 
-/* export async function handleAction(action: NearAction<Transfer>): Promise<void> {
+/* 
+export async function handleAction(action: NearAction<Transfer>): Promise<void> {
   
   logger.info(`Handling action at ${action.transaction.block_height}`);
 
@@ -38,17 +40,19 @@ export async function handleTransaction(transaction: NearTransaction): Promise<v
     sender: action.transaction.signer_id,
     receiver: action.transaction.receiver_id,
     amount: BigInt((action.action as Transfer).deposit.toString()),
-  }); */
+  });
+  */
 
-  export async function handleAction(action: NearAction<FunctionCall>): Promise<void> {
+  export async function handleAction(action: NearAction<Function>): Promise<void> {
     logger.info(`Handling action at ${action.transaction.block_height}`);
+    logger.info(`IN ACTION FUNCTION`);
   
     const actionRecord = NearActionEntity.create({
       id: `${action.transaction.result.id}-${action.id}`,
       sender: action.transaction.signer_id,
-      receiverId: (action.action as FunctionCall).args.toJson().receiver_id.toString(),
-      amount: BigInt((action.action as FunctionCall).args.toJson().amount.toString()),
-      msg: (action.action as FunctionCall).args.toJson().msg.toString(),
+      receiver: (action.action as Function).arguments.toJson().receiver_id.toString(),
+      amount: BigInt((action.action as Function).arguments.toJson().amount.toString()),
+      //msg: (action.action as Function).args.toJson().msg.toString(),
     });
 
   await actionRecord.save();
